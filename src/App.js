d625b6d57efd1fe451679b6164159a4bef4a3087
Rodolfo Fialho da Costa <rodolfofialho905@gmail.com>
2022-03-29 21:12:36 -0300

    finalizada  página inicial frond da aplicação

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+import{ FiSearch } from 'react-icons/fi';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP </h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu cep.."></input>
+        <button className="btn">
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 56565665</h2>
+
+        <span>Rua Horizonte infinito</span>
+        <span>Complemento: Algum lugar </span>
+        <span>Vila Nova </span>
+        <span>São Paulo - SP </span>
+
+
+      </main>
     </div>
   );
 }
